dc8dc5b863058f8df2aa3f2a2bbdda4787d9c07c
okumurashingo <145005269+okumurashingo@users.noreply.github.com>
2026-03-31 16:42:04 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Service Workerを完全無効化：キャッシュ削除後に自分自身を登録解除
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(names => Promise.all(names.map(n => caches.delete(n))))
-      .then(() => self.registration.unregister())
-  );
-});
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
